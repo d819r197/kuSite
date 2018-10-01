8769d6ff2feb7d1426fbfbeb59607b60f66606b8
Blake Rieschick <brieschick@Apples-Mac.local>
2018-09-30 19:16:09 -0500

    Exercise1.V1

---
+++ eecs448/Lab04/Exercise1/index.php
@@ -0,0 +1,15 @@
+
+
+<?php
+  echo "<p>";
+  for (x = 1; x < 100; x++)
+  {
+    for(y = 1; y <= 100; y++)
+    {
+      $product = x*y;
+      echo $product . " ";
+    }
+    echo "<br/>";
+  }
+  echo "</p>";
+?>

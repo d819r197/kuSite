9820c7c375d7bddda5f34eec753074829fb3c742
d819r197 <d819r197@ku.edu>
2018-09-30 19:40:14 -0500

    Exercise1.V1.3

--- eecs448/Lab04/Exercise1/index.php
+++ eecs448/Lab04/Exercise1/index.php
@@ -1,15 +1,10 @@
 <?php
-   echo "<p>Hello, World!</p>";
-   error_reporting(E_ALL);
-   ini_set("display_errors", 1);
-?>
-<?php
   echo "<p>";
-  for (x = 1; x < 100; x++)
+  for ($x = 1; $x < 100; $x++)
   {
-    for(y = 1; y <= 100; y++)
+    for($y = 1; $y <= 100; $y++)
     {
-      $product = x*y;
+      $product = $x*$y;
       echo $product . " ";
     }
     echo "<br/>";

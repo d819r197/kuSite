bc8df6e17b82f0ae92ca3aa5c7203961edfa17af
d819r197 <d819r197@ku.edu>
2018-10-02 16:05:03 -0500

    Exercise3.V1.1

--- eecs448/Lab04/Exercise3/customerBackend.php
+++ eecs448/Lab04/Exercise3/customerBackend.php
@@ -1,3 +1,31 @@
 <?php
+  $user = $_POST["user"];
+  $pass = $_POST["pass"];
+  $total = 0;
 
+  if($_POST["1a"])
+  {
+    $total += $_POST["1a"].value;
+  }
+  if($_POST["1b"])
+  {
+    $total += $_POST["1b"].value;
+  }
+  if($_POST["1c"])
+  {
+    $total += $_POST["1c"].value;
+  }
+  if($_POST["2a"])
+  {
+    $total += $_POST["2a"].value;
+  }
+  if($_POST["2b"])
+  {
+    $total += $_POST["2b"].value;
+  }
+  if($_POST["2c"])
+  {
+    $total += $_POST["2c"].value;
+  }
+  echo "<p>The Total is: " + $total + ".</p>";
 ?>

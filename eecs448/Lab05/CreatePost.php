e09c7742d08a2c323368cf85ee24bfb8a4365de3
d819r197 <d819r197@ku.edu>
2018-10-22 18:53:34 -0500

    Issue with getting php to send to sql db

--- eecs448/Lab05/CreatePost.php
+++ eecs448/Lab05/CreatePost.php
@@ -1,5 +1,38 @@
 <?php
-   echo "<p>The User Name is: /p>" . $_POST["user"];
-   $mysqli = new mysqli("database_URL", "my_user", "my_password", "database_name");
+   echo "<p>The User Name is: " . $_POST["user"] . "</p>";
+   $mysqli = new mysqli("mysql.eecs.ku.edu", "d819r197", "Koo3Kee4", "d819r197");
 
+  /* check connection */
+  if (!$mysqli->connect_errno) {
+      printf("Connection Success!");
+  }
+  else {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();  
+  }
+
+  printf("Starting ADD USER...");
+$user = $_POST["user"];
+$query = "INSERT INTO Users (User_Id)
+VALUES ('userT2')";
+ 
+if ($conn->query($query) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+  printf("done!");
+$conn->close();
+/* 
+  printf("adding user...");
+  
+  if(msqli.query($query)) {
+    printf('New User Added Successful!');
+  }
+  else {
+    printf('New User Added Failed!');
+  }
+  */
+  printf("done!");
 ?>
+

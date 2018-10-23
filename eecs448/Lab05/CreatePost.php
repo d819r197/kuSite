913b0095ff185c9fe54fdec62385a389f449c4ec
d819r197 <d819r197@ku.edu>
2018-10-23 18:47:58 -0500

    Inital Commit to Exercise3

--- eecs448/Lab05/CreatePost.php
+++ eecs448/Lab05/CreatePost.php
@@ -4,27 +4,38 @@
 
   /* check connection */
   if (!$mysqli->connect_errno) {
-      printf("Connection Success!");
+      printf("Connection Success!\n");
   }
   else {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
   }
 
-  printf("Starting ADD USER");
+  printf("Starting ADD USER\n");
 
-  $userQuery = "SELECT * FROM `Users` WHERE `user_id`='" . $user . "';";
-  $userIs = $mysqli->query($query);
-  $numUsersFound = $userIs->num_rows;
+  $userInput = $_POST["user"];
+  $postInput = $_POST["post"];
+
+  if ($userInput == "" || $postInput == "") {
+    echo "<p>Error: Empty Post!</p><br>";
+    return;
+  }
+
+  $userQuery = "SELECT * FROM `Users` WHERE `user_id`='" . $userInput . "';";
+    $userIs = $mysqli->query($userQuery);
+    $numUsersFound = $userIs->num_rows;
+  $postQuery = "SELECT * FROM `Posts`";
+    $postAre = $mysqli->query($postQuery);
+    $numOfPosts = ($postAre->num_rows) + 1;
 
   if ($numUsersFound > 0) {
-    $newPost = 
+    $newPost = "INSERT INTO `Posts`(`post_id`, `content`, `author_id`) VALUES ('" . $numOfPosts . "','" . $postInput . "','" . $userInput ."');";
 
-    if ($mysql_connection->query($query)) {
-        echo "<p>Post was Added</p>";
+    if ($mysqli->query($newPost)) {
+        echo "<p>New Post was Added</p>";
     }
     else {
-        echo "<p>Error</p>";
+        echo "<p>Error: Post could not be added.</p>";
     }
   }
   else {

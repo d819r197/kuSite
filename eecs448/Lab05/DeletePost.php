13391b4479ffb6f30133e653e0ab55bde0dd8581
d819r197 <d819r197@ku.edu>
2018-10-28 18:48:58 -0500

    merge front and back end for EX7

--- eecs448/Lab05/DeletePost.php
+++ eecs448/Lab05/DeletePost.php
@@ -1,6 +1,6 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "d819r197", "Koo3Kee4", "d819r197");
-
+$counter = $_POST("numOfPosts");
 /* check connection */
 if (!$mysqli->connect_errno) {
    printf("Connection Success!\n");
@@ -9,6 +9,9 @@ else {
  printf("Connect failed: %s\n", $mysqli->connect_error);
  exit();
 }
+
+echo "<h3 name='numOfPosts' value='" . $counter . "'>Number of Posts: " . $counter ."</h3>";
+
 $selectedUser = $_POST["userSelect"];
 $postQuery = "SELECT * FROM `Posts` WHERE `Author_Id`='" . $selectedUser . "';";
 $postIs = $mysqli->query($postQuery);

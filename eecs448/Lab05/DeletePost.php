45ffd5d4322720291bf49c07532a2bc3ec170788
d819r197 <d819r197@ku.edu>
2018-10-28 16:58:59 -0500

    inital commit for exercise 7

---
+++ eecs448/Lab05/DeletePost.php
@@ -0,0 +1,27 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "d819r197", "Koo3Kee4", "d819r197");
+
+/* check connection */
+if (!$mysqli->connect_errno) {
+   printf("Connection Success!\n");
+}
+else {
+ printf("Connect failed: %s\n", $mysqli->connect_error);
+ exit();
+}
+$selectedUser = $_POST["userSelect"];
+$postQuery = "SELECT * FROM `Posts` WHERE `Author_Id`='" . $selectedUser . "';";
+$postIs = $mysqli->query($postQuery);
+
+echo "<table>";
+
+while($rowOfPosts = $postIs->fetch_assoc()) {
+  echo "<td>" . $rowOfPosts["Author_Id"] . "</td><br>";
+}
+
+echo "</table>";
+
+/* close connection */
+$mysqli->close();
+
+?>
